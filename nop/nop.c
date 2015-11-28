b0e2daf21e4adeef07dcfcf3d99c5ea5c0357400
paxnil <paxnil@gmail.com>
2015-11-28 21:50:17 +0800

    useless commit

---
+++ nop/nop.c
@@ -0,0 +1,7 @@
+#include <windows.h>
+
+// int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+void _start() {
+    ExitProcess(0);
+//    return 0;
+}
--- scrnoff/scrnoff.c
+++ scrnoff/scrnoff.c
@@ -1,8 +1,8 @@
 #include <windows.h>
 
-int main(int argc, char *argv[]) {
+void _start() {
     Sleep(500);
     SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, 2);
     LockWorkStation();
-    return 0;
+    ExitProcess(0);
 }
